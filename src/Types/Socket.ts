85ccebcda0249f3d6b1a70208677db760d877373
uazapi e afins <augustodesenvolvedor@gmail.com>
2024-06-13 22:34:25 -0300

    localaddress socket

--- src/Types/Socket.ts
+++ src/Types/Socket.ts
@@ -134,4 +134,7 @@ export type SocketConfig = {
 
     /** Socket passthrough */
     socket?: any
+
+    /** Use if your server has multiple IPs and you want to specify the local IP */
+    localAddress?: string,
 }
